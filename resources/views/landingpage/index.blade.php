253cc866deb81a447dae04d637dafaadee4399f0
Dera <deraadijaya@gmail.com>
2023-02-06 13:15:42 +0700

    project pertama

---
+++ resources/views/landingpage/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+</head>
+<body>
+    @include('header.index')
+    <h1 class="h1_style">Halaman Utama</h1>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\HeaderController;
+use App\Http\Controllers\LandingpageController;
+use App\Http\Controllers\LayananController;
+use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [LandingpageController::class, 'index']);
+
+Route::get('/layanan', [LayananController::class, 'index']);
+
+Route::get('/order', [OrderController::class, 'index']);
+
+Route::get('/header', [HeaderController::class, 'index']);
